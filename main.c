3a2d4028cae3470453c700fbdf56e91189e52a0b
architxmittal <architmittal406@gmail.com>
2025-04-11 12:13:13 +0530

    Update and rename .gitignore to main.c

---
+++ main.c
@@ -0,0 +1,63 @@
+//Name
+//
+//
+
+
+#include <stdio.h>
+#include "header.h"
+
+int main() {
+    int choice, year, number, size, i;
+    
+    printf("Choose operation:\n");
+    printf("1. Check leap year\n");
+    printf("2. Reverse an array\n");
+    printf("3. Check strong number\n");
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &choice);
+    
+    if (choice == 1) {
+        // Leap year check
+        printf("Enter a year: ");
+        scanf("%d", &year);
+        
+        if (is_leap_year(year)) {
+            printf("%d is a leap year.\n", year);
+        } else {
+            printf("%d is not a leap year.\n", year);
+        }
+    }
+    else if (choice == 2) {
+        // Array reversal
+        printf("Enter size of array: ");
+        scanf("%d", &size);
+        
+        int arr[size];
+        printf("Enter %d numbers: ", size);
+        for (i = 0; i < size; i++) {
+            scanf("%d", &arr[i]);
+        }
+        reverse_array(arr, size);
+        printf("Reversed array: ");
+        for (i = 0; i < size; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    else if (choice == 3) {
+        // Strong number check
+        printf("Enter a number: ");
+        scanf("%d", &number);
+        
+        if (is_strong_number(number)) {
+            printf("%d is a strong number.\n", number);
+        } else {
+            printf("%d is not a strong number.\n", number);
+        }
+    }
+    else {
+        printf("Invalid choice!\n");
+    }
+    
+    return 0;
+}
